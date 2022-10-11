3a3697d0728804b2c6900316164d14b2035f4b18
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2022-10-11 15:41:18 -0600

    move images into lib folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Mike from '../images/md.png';
+	import Mike from '$lib/images/md.png';
 </script>
 
 <div class="container">
@@ -14,6 +14,6 @@
 		align-items: center;
 		justify-content: center;
 		height: 100vh;
-		background: url('../images/background.jpg') 50% 50% / cover no-repeat;
+		background: url('$lib/images/background.jpg') 50% 50% / cover no-repeat;
 	}
 </style>
